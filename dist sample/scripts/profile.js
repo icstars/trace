61fa8858066570e4ac4a0cfdfad5088f26c22c33
Dan Rodgers <drodgers@icstars.org>
2017-05-28 12:50:05 -0500

    profile page now loads employee info when a SIDEBAR LI is selected

--- dist sample/scripts/profile.js
+++ dist sample/scripts/profile.js
@@ -9,5 +9,61 @@ $(document).ready(function() {
     emp = data[i];
     employees[emp._id] = emp;
   }
-  
+
+  $('#save-btn').hover(function() {
+    $(this).text(' Save');
+  }, function() {
+    $(this).text('');
+  })
+
+  $('#edit-btn').hover(function() {
+    $(this).text(' Edit');
+  }, function() {
+    $(this).text('');
+  })
+
+  $('#rate-btn').hover(function() {
+    $(this).text(' Rate');
+  }, function() {
+    $(this).text('');
+  })
+
+  $('#share-btn').hover(function() {
+    $(this).text(' Share');
+  }, function() {
+    $(this).text('');
+  })
+
+  $('#delete-btn').hover(function() {
+    $(this).text(' Delete');
+  }, function() {
+    $(this).text('');
+  })
+
+  //Load employee info when selected in SIDEBAR
+  $('#side-bar-list li').click(function() {
+    //Some fields are commented because they're not yet in the sample data
+    endex = $(this).data('e_id');
+    $('#profile-header').text(employees[endex].fname + ' ' + employees[endex].lname);
+    $('#id_field').text(employees[endex]._id);
+    $('#gender_field').text(employees[endex].gender);
+    $('#bday_field').text(employees[endex].startDate);
+    $('#pos_field').text(employees[endex].department);
+    // $('#dep_field').text(employees[endex].department);
+    $('#startDate_field').text(employees[endex].startDate);
+    // $('#location_field').text(employees[endex]._id);
+    //CONTACT INFO
+    // $('#email_field').text(employees[endex].startDate);
+    // $('#phone_field').text(employees[endex].startDate);
+    // $('#ext_field').text(employees[endex].startDate);
+    //RATING HISTORY
+    populateRatings(endex);
+  })
+
+  var populateRatings = function(endex){
+    $('#ratings-history-list').empty();
+    for (var i in employees[endex].ratings) {
+      $('#ratings-history-list').append("<tr><td class='toggle'><i class='icon-expand'></i></td><td class='h2 type-centeralign'>"+employees[endex].ratings[i].date+"</td><td class='h2 type-centeralign'>"+employees[endex].ratings[i].potential+"</td><td class='h2 type-centeralign'>"+employees[endex].ratings[i].performance+"</td><td class='h2 type-centeralign'>"+employees[endex].ratings[i].notes+"</td><td class='h2 type-centeralign'>"+employees[endex].refersTo+"</td></tr>");
+    }
+  }
 });
