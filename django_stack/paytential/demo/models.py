bd61c3b93c011e8edee13ac0a0fd52b108f853bd
Daniel Rodgers <Daniel Rodgers>
2017-06-02 11:22:49 -0500

    finisned population script

--- django_stack/paytential/demo/models.py
+++ django_stack/paytential/demo/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Employee(models.Model):
     employee_id = models.IntegerField(primary_key=True)
-    manager_id = models.ForeignKey( 'self', null=True )
+    is_manager = models.BooleanField()
     first_name = models.CharField(max_length=264)
     last_name = models.CharField(max_length=264)
     gender = models.CharField(max_length=264)
@@ -10,20 +10,18 @@ class Employee(models.Model):
     hire_date = models.DateField()
     termination_date = models.DateField(null=True)
     position = models.CharField(max_length=264)
-    department = models.CharField(max_length=264)
     operational_unit_id = models.IntegerField()
     email = models.EmailField(max_length=264, unique=True)
     phone = models.IntegerField(unique=True)
-    extension = models.IntegerField()
 
     def __str__(self):
         return self.f_name + " " + self.l_name
 
 class Management(models.Model):
-    employee_id = models.ForeignKey(Employee, related_name='Employee.mployee_id+')
-    manager_id = models.ForeignKey(Employee, related_name='Employee.manager_id+')
+    employee_id = models.ForeignKey(Employee, related_name='Employee.employee_id+')
+    manager_id = models.ForeignKey(Employee, related_name='Employee.employee_id+')
     start_date = models.DateField()
-    end_date = models.DateField()
+    end_date = models.DateField(null=True)
 
     def __str__(self):
         return str(self.manager_id + ":" + self.employee_id)
--- django_stack/paytential/populate_demo.py
+++ django_stack/paytential/populate_demo.py
@@ -11,7 +11,51 @@ from demo.models import Employee,Management,Rating
 from faker import Faker
 
 fakegen = Faker()
-fnames = ['']
+positions = ['Cashier','Cook','Front','Driver']
 
-def add_names():
-    n = Employee.objects.get_or_create(first_name=random.choice(fnames))[0]
+#generate a random position
+def pick_pos():
+    p = Employee.objects.get_or_create(position=random.choice(positions))[0]
+    p.save()
+    return p
+
+operational_unit_id = fakegen.ean(length=3)
+
+def populate(N=5):
+    for i in range(N):
+    #Employee
+        employee_id = i
+        #manager_id = False
+        first_name = fakegen.first_name()
+        last_name = fakegen.last_name()
+        gender = fakegen.gender()
+    #make y/m/d
+        year = random.choice(range(1975, 2001))
+        month = random.choice(range(1, 13))
+        day = random.choice(range(1, 31))
+    #combine y/m/d into a datetime
+        birth_date = datetime(year, month, day)
+    #hire employee on their 16th birthday
+        hire_date = datetime(year + 16, month, day)
+    #we NEVER fire employees
+        termination_date = null
+        position = pick_pos()
+        email = fakegen.safe_email()
+        phone = fakegen.phone_number()
+    #Management
+        start_date = datetime(year + random.choice(range(5)), month, day)
+        end_date = null
+    #Rating
+        potential = random.choice(range(1,11))
+        performance = random.choice(range(1,11))
+        timestamp = datetime(year + random.choice(range(5,10)), month, day)
+        notes = fakegen.text(max_nb_chars=200)
+
+    #Create a fake Employee
+        emp = Employee.objects.get_or_create(employee_id,manager_id,first_name,last_name,gender,birth_date,hire_date,termination_date,position,email,phone)
+    #Create managment relationships for that Employee
+        for i in random.choice(range(1,3))
+            mng = Management.objects.get_or_create(employee_id,random.choice(range(30,33)),start_date,end_date)
+
+        for i in random.choice(range(1,9))
+            ratings = Rating.objects.get_or_create(employee_id,random.choice(range(30,33)),potential,performance,timestamp,notes)
