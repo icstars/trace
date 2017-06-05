b082252894e237fdc928ef38086cb35a7a2a2e38
Daniel Rodgers <Daniel Rodgers>
2017-06-05 15:29:30 -0500

    finalized model, got population script working for employees, and updated README

--- django_stack/paytential/demo/models.py
+++ django_stack/paytential/demo/models.py
@@ -1,21 +1,21 @@
 from django.db import models
-import django
+from django.utils import timezone
 
 class Employee(models.Model):
-    employee_id = models.IntegerField(unique=False,null=True)#should be unique and non null
+    employee_id = models.PositiveIntegerField(unique=True,default=0)
     managment_relationships = models.ManyToManyField('self', through='Supervision',symmetrical=False,related_name='refers_to')
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     gender = models.CharField(max_length=10)
-    birth_date = models.DateField(null=True)
-    hire_date = models.DateField(null=True)
+    birth_date = models.DateField(default=timezone.now)
+    hire_date = models.DateField(default=timezone.now)
     termination_date = models.DateField(null=True,default=None)
-    position = models.CharField(max_length=264)
-    operational_unit_id = models.IntegerField(null=True)
-    email = models.EmailField(max_length=264)
+    position = models.CharField(max_length=30)
+    operational_unit_id = models.PositiveIntegerField(default=0)
+    email = models.EmailField(max_length=100)
     phone = models.CharField(max_length=20)
-    created_at = models.DateTimeField(default=django.utils.timezone.now)
-    updated_at = models.DateTimeField(default=django.utils.timezone.now)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
@@ -23,27 +23,27 @@ class Employee(models.Model):
 class Manager(Employee):
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=100)
-    permissions_level = models.IntegerField()
+    permissions_level = models.PositiveIntegerField(default=0)
 
 class Supervision(models.Model):
-    subordiante = models.ForeignKey(Employee, related_name='employees')
-    supervisor = models.ForeignKey(Manager, related_name='supervisors')
-    start_date = models.DateField()
-    end_date = models.DateField(null=True)
-    created_at = models.DateTimeField(default=django.utils.timezone.now)
-    updated_at = models.DateTimeField(default=django.utils.timezone.now)
+    subordiante = models.ForeignKey('Employee', related_name='employees')
+    supervisor = models.ForeignKey('Manager', related_name='supervisors')
+    start_date = models.DateField(timezone.now)
+    end_date = models.DateField(null=True, default=None)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return str(self.manager_id + ":" + self.employee_id)
 
 class Rating(models.Model):
-    subordiante = models.ForeignKey(Supervision, related_name='employees', null=True)
-    supervisor = models.ForeignKey(Supervision, related_name='supervisors', null=True)
-    potential = models.IntegerField()
-    performance = models.IntegerField()
+    subordiante = models.ForeignKey('Supervision', related_name='employees', null=True)
+    supervisor = models.ForeignKey('Supervision', related_name='supervisors', null=True)
+    potential = models.PositiveSmallIntegerField(default=0)
+    performance = models.PositiveSmallIntegerField(default=0)
     notes = models.TextField(max_length=1000)
-    created_at = models.DateTimeField(default=django.utils.timezone.now)
-    updated_at = models.DateTimeField(default=django.utils.timezone.now)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return str(self.potential + ":" + self.performance)
--- django_stack/paytential/populate_demo.py
+++ django_stack/paytential/populate_demo.py
@@ -6,67 +6,28 @@ django.setup()
 
 ##FAKE POP START
 
-import random
-from demo.models import Employee,Management,Rating
+from demo.models import Employee,Manager,Supervision,Rating
 from faker import Faker
-from datetime import date
 
 fakegen = Faker()
-positions = ['Cashier','Cook','Front','Driver']
-
-#generate a random position
-def pick_pos():
-    p = Employee.objects.get_or_create(position=random.choice(positions))[0]
-    p.save()
-    return p
 
 def populate(N=5):
-#Create 3 Managers
-    mstart = fakegen.date()
-    mend = fakegen.date()
-    m1 = Employee.objects.get_or_create(employee_id=34161727,is_manager=True,first_name='Joe',last_name='McManager',gender='M',\
-    birth_date=mstart,hire_date=mend,termination_date=None,position='District Manager',operational_unit_id=148,email=fakegen.company_email(),\
-    phone=fakegen.phone_number(),create_date=date.today())[0]
-    m2 = Employee.objects.get_or_create(employee_id=44613589,is_manager=True,first_name='Sue',last_name='McManager',gender='F',\
-    birth_date=mstart,hire_date=mend,termination_date=None,position='District Manager',operational_unit_id=403,email=fakegen.company_email(),\
-    phone=fakegen.phone_number(),create_date=date.today())[0]
-    m3 = Employee.objects.get_or_create(employee_id=51357643,is_manager=True,first_name='Faker',last_name='_',gender='M',\
-    birth_date=mstart,hire_date=mend,termination_date=None,position='District Manager',operational_unit_id=284,email=fakegen.company_email(),\
-    phone=fakegen.phone_number(),create_date=date.today())[0]
     for i in range(N):
-        create_date = date.today()
-    #Employee
-        employee_id = fakegen.ean(length=8)
-        is_manager = False
-        first_name = fakegen.first_name()
-        last_name = fakegen.last_name()
-        gender = random.choice(['M','F'])
-    #employees are at least 16yrs old
-        birth_date = fakegen.date()
-        hire_date = fakegen.date()
-    #we NEVER fire employees
-        termination_date = None
-        position = pick_pos()
-        operational_unit_id = random.choice([148,403,284])
-        email = fakegen.company_email()
+        emp_id = i
+        fname = fakegen.first_name()
+        lname = fakegen.last_name()
+        gender = fakegen.random_element(elements=('F','M'))
+        # bdate = models.DateField(default=timezone.now)
+        # hdate = models.DateField(default=timezone.now)
+        # termination_date = models.DateField(null=True,default=None)
+        pos = fakegen.random_element(elements=('Cashier','Cook','Front','Driver'))
+        opuid = fakegen.random_int(min=1,max=3)
+        email = fakegen.email()
         phone = fakegen.phone_number()
 
-    #Create a fake Employee
-        emp = Employee.objects.get_or_create(employee_id=employee_id,first_name=first_name,last_name=last_name,gender=gender,\
-        birth_date=birth_date,hire_date=hire_date,position=position,operational_unit_id=operational_unit_id,email=email,\
-        phone=phone,create_date=create_date)[0]
-    #Create managment relationships for that Employee
-        for i in range(3):
-            manager_id = [m1,m2,m3][i]
-            start_date = fakegen.date()
-            end_date = None
-            mng = Management.objects.get_or_create(employee_id=emp,manager_id=manager_id,start_date=start_date,end_date=end_date,create_date=create_date)[0]
-        #Create ratings for that Employee
-            for i in range(4):
-                potential = random.choice(range(1,11))
-                performance = random.choice(range(1,11))
-                notes = fakegen.text(max_nb_chars=200)
-                ratings = Rating.objects.get_or_create(employee_id=mng,manager_id=mng,potential=potential,performance=performance,notes=notes,create_date=create_date)[0]
+        emp = Employee.objects.get_or_create(employee_id=emp_id,first_name=fname,last_name=lname,gender=gender,position=pos,operational_unit_id=opuid,email=email,phone=phone)[0]
 
 if __name__ == '__main__':
+    print('Populating...')
     populate(1)
+    print('Finished!')
