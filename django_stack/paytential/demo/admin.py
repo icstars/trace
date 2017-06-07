3f1b1267ae3a2ba85ac61194247ee1e5d829b7e5
Daniel Rodgers <Daniel Rodgers>
2017-06-06 22:51:33 -0500

    data model and population are finalized (for real this time)

--- django_stack/paytential/demo/admin.py
+++ django_stack/paytential/demo/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from demo.models import Employee,Manager,Supervision,Rating
+from demo.models import Employee,Manager,Location,Location_lookup,Supervision,Rating
 # Register your models here.
 admin.site.register(Employee)
 admin.site.register(Manager)
+admin.site.register(Location)
+admin.site.register(Location_lookup)
 admin.site.register(Supervision)
 admin.site.register(Rating)
--- django_stack/paytential/populate_demo.py
+++ django_stack/paytential/populate_demo.py
@@ -6,28 +6,67 @@ django.setup()
 
 ##FAKE POP START
 
-from demo.models import Employee,Manager,Supervision,Rating
+from demo.models import Employee,Manager,Location,Location_lookup,Supervision,Rating
 from faker import Faker
 
 fakegen = Faker()
 
-def populate(N=5):
-    for i in range(N):
-        emp_id = i
+def populate(M=3, N=5):
+    i = 0
+    #Create 3 Managers
+    while i < (M + M * N):
         fname = fakegen.first_name()
         lname = fakegen.last_name()
+        uname = fname[0] + lname
         gender = fakegen.random_element(elements=('F','M'))
-        # bdate = models.DateField(default=timezone.now)
-        # hdate = models.DateField(default=timezone.now)
-        # termination_date = models.DateField(null=True,default=None)
-        pos = fakegen.random_element(elements=('Cashier','Cook','Front','Driver'))
-        opuid = fakegen.random_int(min=1,max=3)
-        email = fakegen.email()
-        phone = fakegen.phone_number()
+        bdate = fakegen.date()
+        hdate = fakegen.date()
+        loc_id = i+1
+        loc_name = fakegen.company()
+        email= fname[0].lower()+lname.lower()+"@companyname.org"
 
-        emp = Employee.objects.get_or_create(employee_id=emp_id,first_name=fname,last_name=lname,gender=gender,position=pos,operational_unit_id=opuid,email=email,phone=phone)[0]
+        mng = Manager.objects.get_or_create(username=uname,password='123',\
+        permissions_level=1,employee_id=i,first_name=fname,last_name=lname,\
+        gender=gender,birth_date=bdate,hire_date=hdate,position='Manager',\
+        email=email,phone=fakegen.phone_number())[0]
+
+        loc = Location.objects.get_or_create(assigned_employee=mng,location_id=loc_id,location_name=loc_name)[0]
+        i+=1
+        #Create N Employees for each manager
+        j = i
+        while i < j+N:
+            emp_id = i
+            fname = fakegen.first_name()
+            lname = fakegen.last_name()
+            gender = fakegen.random_element(elements=('F','M'))
+            bdate = fakegen.date()
+            hdate = fakegen.date()
+            #termination_date = models.DateField(null=True,default=None)
+            pos = fakegen.random_element(elements=('Cashier','Cook','Front','Driver'))
+            email= fname[0].lower()+lname.lower()+"@companyname.org"
+            phone = fakegen.phone_number()
+
+            emp = Employee.objects.get_or_create(employee_id=emp_id,first_name=fname,\
+            last_name=lname,gender=gender,birth_date=bdate,hire_date=hdate,position=pos,\
+            email=email,phone=phone)[0]
+            #Location
+            loc_id = fakegen.random_int(min=1,max=3)
+            loc_name = fakegen.company()
+
+            loc = Location.objects.get_or_create(assigned_employee=emp,location_id=loc_id,location_name=loc_name)[0]
+            #Supervision
+            sdate = fakegen.date()
+
+            sprvs = Supervision.objects.get_or_create(subordinate=emp,supervisor=mng,start_date=sdate)[0]
+            #Rating
+            pot = fakegen.random_int(1,10)
+            perf = fakegen.random_int(1,10)
+            notes = fakegen.text(max_nb_chars=50)
+
+            rat = Rating.objects.get_or_create(managment_relationship=sprvs,potential=pot,performance=perf,notes=notes)[0]
+            i+=1
 
 if __name__ == '__main__':
     print('Populating...')
-    populate(1)
+    populate(1,1)
     print('Finished!')
