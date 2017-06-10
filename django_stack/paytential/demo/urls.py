0e7ac892c81ddeccdec44c8e9ccbad0da4bd95eb
Sigil814 <cmartinez@icstars.org>
2017-06-09 19:36:19 -0500

    added links for all pages

--- django_stack/paytential/demo/urls.py
+++ django_stack/paytential/demo/urls.py
@@ -3,5 +3,10 @@ from demo import views
 
 urlpatterns = [
     url(r'^$',views.index,name='index'),
-    url(r'^$',views.index,name='profile'),
+    url(r'^profile',views.profile,name='profile'),
+    url(r'^login',views.login,name='login'),
+    url(r'^evaluation',views.evaluation,name='evaluation'),
+    url(r'^registration',views.registration,name='registration'),
+    url(r'^help',views.help,name='help'),      
+
 ]
--- django_stack/paytential/demo/views.py
+++ django_stack/paytential/demo/views.py
@@ -4,9 +4,21 @@ from demo.models import Employee,Manager,Location,Location_lookup,Supervision,Ra
 # Create your views here.
 def index(request):
     employee_list = Employee.objects.order_by('first_name')
-    ratings = Employee.objects.managment_relationships()
+
     employee_dict = {'employees': employee_list }
     return render(request,'demo/dashboard.html',context=employee_dict)
 
 def profile(request, employee_id):
     return render(request,'demo/profile.html')
+
+def login(request):
+    return render(request,'demo/login.html')
+
+def evaluation(request):
+    return render(request,'demo/evaluation.html')
+
+def help(request):
+    return render(request,'demo/help.html')
+
+def registration(request):
+    return render(request,'demo/registration.html')
