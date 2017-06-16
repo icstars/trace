1957cb3b7f9cd4ff8d1ce348d9b4fc6d93c741a5
Dan Rodgers <drodgers@icstars.org>
2017-06-16 10:59:57 -0500

    added name to eval page

--- django_stack/paytential/static/scripts/evaluation.js
+++ django_stack/paytential/static/scripts/evaluation.js
@@ -1,125 +1,25 @@
 $(document).ready(function() {
+  var loadEmployeeInfo = function(id){
+    var employee = {};
+    $.ajax({
+      type: 'GET',
+      url: '/api/employees/'+id,
+      success: function(data) {
+        employee = data;
+      }
+    }).done(function(){
+      $('#eval-header').text(employee.details[0].first_name + ' ' + employee.details[0].last_name);
+    })
 
-  //Read in sample data
-  var data = JSON.parse(jsonData);
-  var employees = {};
-
-  for (var i = 0, emp; i < data.length; i++) {
-    emp = data[i];
-    employees[emp._id] = emp;
-  }
-  //Populate SIDEBAR
-  // for (var i in employees) {
-  //   $('#side-bar-list').append("<li><span>" + employees[i].fname + " " + employees[i].lname + "</span></li>");
-  // }
-
-  //KEEPS SIDEBAR IN SHAPE AS WINDOW RESIZES
-  //SIDEBAR can't be dependant on the grid as it is only on the dashboard
-  // TODO fix this
-
-  $('#side-bar-list').css('height', $('#eval-section').width());
-  $(window).resize(function() {
     $('#side-bar-list').css('height', $('#eval-section').width());
-  })
-
-  //Tag SIDEBAR items with e_id
-  $('#side-bar-list li').each(function(i) {
-    $(this).data('e_id', employees[i]._id);
-  })
-
-  $('#side-bar-list li').click(function(e) {
-    //TODO link to profile page based on e_id
-    if( $(this).hasClass('selected') ){
-      $(this).removeClass('selected');
-    } else {
-      $(this).addClass('selected');
-    }
-    updateSideBarBtns();
-  })
-  //toggles disabled on SIDEBAR buttons that require selected employees
-  var updateSideBarBtns = function(){
-    if($('#side-bar-list li.selected').length > 0){//checks if any side-bar-list items are selected
-      $('#eval-btn, #profile-btn').removeClass('disabled');
-    } else {
-      $('#eval-btn, #profile-btn').addClass('disabled');
-    }
-    if($('#side-bar-list li').filter(':hidden').length > 0){//checks if any side-bar-list items are hidden
-      $('#reset-btn').removeClass('disabled');
-    } else {
-      $('#reset-btn').addClass('disabled');
-    }
+    $(window).resize(function() {
+      $('#side-bar-list').css('height', $('#eval-section').width());
+    })
   }
 
-  //Display button text on hover
-  //$().hover(mouse in, mouse out)
-  $('#eval-btn').hover(function() {
-    $(this).text(' Rate');
-  },function() {
-    $(this).text('');
-  })
+  if (window.sessionStorage.getItem('id') !== null){
+    var e_id = window.sessionStorage.getItem('id');
+    loadEmployeeInfo(e_id);
+  }
 
-  $('#checklist-btn').hover(function() {
-    if($(this).hasClass('active')){
-      $(this).text(' Clear All');
-    } else {
-      $(this).text(' Select All');
-    }
-  },function() {
-    $(this).text('');
-  })
-
-  $('#profile-btn').hover(function() {
-    $(this).text(' Profile');
-  },function() {
-    $(this).text('');
-  })
-
-  $('#reset-btn').hover(function() {
-    $(this).text(' Reset');
-  },function() {
-    $(this).text('');
-  })
-//SIDEBAR button click functions
-  $('#eval-btn').click(function() {
-    if($(this).hasClass('disabled'))
-      return;//prevents button from working if it should be disabled
-    alert("Whoops this isn't implemented yet");
-    //TODO load eval page w/ e_id from selected
-  })
-  //seleects only the employees that are currently visible in the side-bar-list
-  $('#checklist-btn').click(function() {
-    if($(this).hasClass('disabled'))
-      return;//prevents button from working if it should be disabled
-    if($(this).hasClass('active')){
-      $('#side-bar-list li').filter(':visible').removeClass('selected');
-      $(this).removeClass('active');
-    } else {
-      $('#side-bar-list li').filter(':visible').addClass('selected');
-      $(this).addClass('active');
-    }
-    if($(this).hasClass('active')){
-      $(this).text(' Clear All');
-      $(this).removeClass('icon-checklist');
-      $(this).addClass('icon-list');
-    } else {
-      $(this).text(' Select All');
-      $(this).removeClass('icon-list');
-      $(this).addClass('icon-checklist');
-    }
-    updateSideBarBtns();
-  })
-
-  $('#profile-btn').click(function() {
-    if($(this).hasClass('disabled'))
-      return;//prevents button from working if it should be disabled
-    alert("Whoops this isn't implemented yet");
-    //TODO load profile page w/ e_id from selected
-  })
-
-  $('#reset-btn').click(function() {
-    if($(this).hasClass('disabled'))
-      return;//prevents button from working if it should be disabled
-    $('#side-bar-list li').show();
-    updateSideBarBtns();
-  })
 });
